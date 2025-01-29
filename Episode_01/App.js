96ecee562ebb24eee9300a0cab26f2bcc713be3e
Himanshu Verma <kumarsurajverma6001@gmail.com>
2025-01-29 12:05:24 +0530

    Ep.01 Part4

--- Episode_01/App.js
+++ Episode_01/App.js
@@ -6,6 +6,34 @@ const heading = React.createElement(
   },
   'Hellow World from React!'
 );
+
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child' }, [
+    React.createElement(
+      'h1',
+      {},
+      'Hellow World from React in nested components'
+    ),
+    React.createElement(
+      'h2',
+      {},
+      'Hellow World from React in nested components siblings'
+    ),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement(
+      'h1',
+      {},
+      'Hellow World from React in nested components'
+    ),
+    React.createElement(
+      'h2',
+      {},
+      'Hellow World from React in nested components siblings'
+    ),
+  ]),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(parent);
