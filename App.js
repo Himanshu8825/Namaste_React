dd0e88b0a0cb104234658f8c7ee4c9f6b133aef9
Himanshu Verma <kumarsurajverma6001@gmail.com>
2025-01-29 11:43:10 +0530

    Ep.02 Part 3

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement(
+  'h1',
+  {
+    id: 'title',
+    xyz: 'attribute',
+  },
+  'Hellow World from React!'
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
